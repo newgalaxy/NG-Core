1cfcaf1ae314e9d6b0307d3a7a402db22501e66a
newgalaxy <fgs.newgalaxy@gmail.com>
2014-01-13 19:50:12 -0800

    Base Files 0.1.2

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+if(!defined("INSIDE")){ header("location:./../"); }
+$dbsettings = Array(
+"server"     => "localhost", // MySQL server name.
+"user"       => "root", // MySQL username.
+"pass"       => "root", // MySQL password.
+"name"       => "newgalaxy", // MySQL database name.
+"prefix"     => "ngc_", // Tables prefix.
+"secretword" => "XGProyect928750968"); // Cookies.
+?>
